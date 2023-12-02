f8ed423571a7d7327f5e1ebfce54994b6525fe11
sridevkp <69075577+sridevkp@users.noreply.github.com>
2023-12-02 22:38:09 +0530

    added more functionalities

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -1,14 +1,23 @@
 import './invoice.css'
 import Barcode from 'react-barcode'
 
-const Invoice = ({ invoiceNumber, id, company }) => {
+const Invoice = ({ invoiceNumber, date, id, company, billedTo, content, ps, innerRef, tax, discount }) => {
+    var subtotal = 0
+
+    content.forEach( item => {
+        subtotal += item.price * item.qty
+    });
+
+    const calculatedDiscount = Math.min( discount.max, subtotal * discount.percent/100)
+    const calculatedTax = Number(((subtotal-calculatedDiscount) * tax /100).toFixed(1))
+
     return (
-        <div className="container">
+        <div className="container" ref={innerRef}>
             <div className='header'><h1>INVOICE</h1></div>
-            <div className='details'>
+            <div className='section details'>
                 <div className='company-info'>
                     <div className='brand'>
-                        <img src={company.logo} height={70}></img>
+                        <img src={company.logo} height={70} alt="company logo"></img>
                         <h2 className='brand-name'>{company.name}</h2>
                     </div>
                     
@@ -21,23 +30,90 @@ const Invoice = ({ invoiceNumber, id, company }) => {
                 </div>
                 <div className='invoice-info'>
                     <table>
-                        <tr>
-                            <td className='label'>Date Added</td>   <td>: {new Date().toLocaleDateString()}</td>                          
-                        </tr>
-                        <tr>
-                            <td className='label'>Invoice No</td>   <td> <Barcode value={invoiceNumber} width={1.5} height={60} ></Barcode> </td>
-                        </tr>
-                        <tr>
-                            <td className='label'>Order ID</td>   <td>: 1</td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td className='label'>Date Added</td><td>: {date.toLocaleDateString()}</td>                          
+                            </tr>
+                            <tr>
+                                <td className='label'>Invoice No</td><td> <Barcode value={String(invoiceNumber)} width={1.2} height={50} ></Barcode> </td>
+                            </tr>
+                            <tr>
+                                <td className='label'>Order ID</td><td>: {id}</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
-            <div className='billing-info'>
 
+            <div className='section billing-info'>
+                <div className='sub-header'><h4>BILLED TO:</h4></div>
+                <p>
+                    {billedTo.name}<br/>
+                    {billedTo.address}<br/>
+                    {billedTo.phone}<br/>
+                    {billedTo.email}<br/>
+                </p>
             </div>
-            <div className='content'>
 
+            <div className='section content'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Item</th>
+                            <th>Price</th>
+                            <th>Qty</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            content.map( (item, index) => {
+                                return (<tr key={index}>
+                                    <td>{index+1}.</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}</td>
+                                    <td>{item.qty}</td>
+                                    <td>{item.qty*item.price}</td>
+                                </tr>)
+                            })
+                        }
+
+                        <tr className='subtotal'>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td className='h5'>Subtotal</td>
+                            <td className='h5'>{subtotal}</td>
+                        </tr>
+                        <tr className='imp discount'>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td className='h5'>Discount</td>
+                            <td className='h5'>{calculatedDiscount}</td>
+                        </tr>
+                        <tr className='imp tax'>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td className='h5'>Tax({tax}%)</td>
+                            <td className='h5'>{calculatedTax}</td>
+                        </tr>
+                        <tr className='imp total'>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td className='h4'>Total</td>
+                            <td className='h4'>{subtotal-calculatedDiscount+calculatedTax}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className='section ps'>
+                <h5>{ps.title}</h5>
+                {ps.note}
             </div>
         </div>
     )
